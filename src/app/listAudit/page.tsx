72688edc0f257cd4bc31f816a4fbaffbe7998bf6
migayalach <ayalachavezmiguel@gmail.com>
2024-12-03 00:03:53 -0400

    structure project

---
+++ src/app/listAudit/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function page() {
+  return (
+    <div>
+      <h1>{"Hello I'm list Audit"}</h1>
+    </div>
+  );
+}
+
+export default page;
---
+++ src/app/products/[idProduct]/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function page({ params }) {
+  return (
+    <div>
+      <h1>{"Hello I'm product id"}</h1>
+    </div>
+  );
+}
+
+export default page;
