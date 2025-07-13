c9b5e2aaf1d67ebf6b45d7dd1c99529210ad131b
migayalach <ayalachavezmiguel@gmail.com>
2025-07-13 00:18:06 -0400

    craeted new components

---
+++ src/components/about/imageAbout/ImageAbout.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ImageAbout() {
+  return (
+    <div>ImageAbout</div>
+  )
+}
+
+export default ImageAbout
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,8 @@ import NavBar from "./navBar/NavBar";
 import Search from "./search/Search";
 import Loading from "./loading/Loading";
 import Information from "./information/Information";
+import About from "./about/About";
+import Footer from "./footer/Footer";
+import Instructions from "./instructions/Instructions";
 
-export { NavBar, Search, Loading, Information };
+export { NavBar, Search, Loading, Information, About, Footer, Instructions };
