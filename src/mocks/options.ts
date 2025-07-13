5553a5b0e6e76ecb88d380ed8fbb77e1af050d67
migayalach <ayalachavezmiguel@gmail.com>
2025-07-12 23:12:56 -0400

    craete mocks

---
+++ src/mocks/options.ts
@@ -0,0 +1,13 @@
+export const Qualitymp3 = [
+  { quality: "Low", format: "128k" },
+  { quality: "High", format: "256k" },
+  { quality: "Best", format: "320k" },
+];
+
+export const Qualitymp4 = [
+  { quality: "Low", format: "144p" },
+  { quality: "Standard", format: "360p" },
+  { quality: "SD", format: "480p" },
+  { quality: "HD", format: "720p" },
+  { quality: "Full HD", format: "1080p" },
+];
