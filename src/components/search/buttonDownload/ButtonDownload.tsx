4663494d3e16a050fc62cec15ee9d397b1ee5507
migayalach <ayalachavezmiguel@gmail.com>
2025-07-13 20:14:40 -0400

    component only view ready, need add backend

--- src/components/search/buttonDownload/ButtonDownload.tsx
+++ src/components/search/buttonDownload/ButtonDownload.tsx
@@ -1,7 +1,8 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { DownloadOutlined } from "@ant-design/icons";
-import { Button } from "antd";
+import { Button, Modal } from "antd";
+import ProgressAction from "@/components/progressAction/ProgressAction";
 
 interface InputDataDownload {
   url: string;
@@ -10,17 +11,41 @@ interface InputDataDownload {
 }
 
 function ButtonDownload({ url, quality, format }: InputDataDownload) {
-  // console.log(url, quality, format);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [dispatch, setDispatch] = useState(false);
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
 
   const downloadFile = () => {
-    console.log(":D");
+    setIsModalOpen(true);
+    setDispatch(true);
   };
+
+  useEffect(() => {
+    if (isModalOpen) {
+      setTimeout(() => {
+        setIsModalOpen(false);
+        setDispatch(false);
+      }, 5000);
+    }
+  }, [dispatch, isModalOpen]);
 
   return (
     <div>
       <Button type="primary" icon={<DownloadOutlined />} onClick={downloadFile}>
         Download
       </Button>
+      <Modal
+        title="Please wait a moment, your song is downloading."
+        closable={false}
+        open={isModalOpen}
+        onOk={handleOk}
+        footer={null}
+      >
+        <ProgressAction />
+      </Modal>
     </div>
   );
 }
