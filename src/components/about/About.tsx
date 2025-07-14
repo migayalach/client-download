cb86dc17b1f53acd565713f5354d6d19e4b98fae
migayalach <ayalachavezmiguel@gmail.com>
2025-07-13 20:19:08 -0400

    render image with NextTool and render other data

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { info } from "@/mocks/instruction";
+import Image from "next/image";
 
 function About() {
   return (
-    <div>About</div>
-  )
+    <>
+      {info.map(({ image, title, description }, index) => {
+        return (
+          <div key={index}>
+            <div>
+              <Image src={image} alt={title} width={40} height={40} />
+            </div>
+            <div>{title}</div>
+            <div>{description}</div>
+          </div>
+        );
+      })}
+    </>
+  );
 }
 
-export default About
+export default About;
