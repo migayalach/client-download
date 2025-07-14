77f08c76748445f9f882f806fdc32bc3899cb6f1
migayalach <ayalachavezmiguel@gmail.com>
2025-07-13 21:38:34 -0400

    created text

---
+++ src/mocks/about.ts
@@ -0,0 +1,17 @@
+export const textAbout1 = [
+  { text: "Open your browser and visit YouTube." },
+  { text: "Copy and paste the video link into the search box." },
+  { text: "Press start." },
+  { text: "Select an MP3 or MP4 format and select a quality to download." },
+  { text: "The conversion process will take a few seconds to complete." },
+  { text: "Click the Download button and save it to your device." },
+];
+
+export const textAbout2 = [
+  { text: "Download videos and audio easily." },
+  {
+    text: "You can choose your favorite formats such as MP3, MP4, HD o Full HD.",
+  },
+  { text: "Convert videos and MP3s with high-speed quality." },
+  { text: "No software or application installation is required." },
+];
