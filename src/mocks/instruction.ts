63fb791a5b52191baf77c631b19e40f2da9dca6d
migayalach <ayalachavezmiguel@gmail.com>
2025-07-13 20:12:14 -0400

    data to instructions

---
+++ src/mocks/instruction.ts
@@ -0,0 +1,24 @@
+import cloud from "@/images/cloud-arrow-down-fill.svg";
+import film from "@/images/film.svg";
+import gift from "@/images/gift-fill.svg";
+
+export const info = [
+  {
+    image: gift,
+    title: "Free YouTube video downloader",
+    description:
+      "This is free to use, you don't have to pay anything for it's use.",
+  },
+  {
+    image: film,
+    title: "Supports multiple files",
+    description:
+      "It offers different formats, such as MP4 and MP3. You can select the format you need to download.",
+  },
+  {
+    image: cloud,
+    title: "Quick and easy conversion",
+    description:
+      "With our advanced technology, you won't have to wait to download YouTube videos.",
+  },
+];
