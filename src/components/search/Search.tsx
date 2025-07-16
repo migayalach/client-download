a84013e3cd96fb49b35c17ae9fd1f207a5e52ef3
migayalach <ayalachavezmiguel@gmail.com>
2025-07-15 23:48:32 -0400

    added styles

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import Find from "./find/Find";
 function Search() {
-  return <Find />;
+  return (
+    <div id="find" className="mt-4 mb-4">
+      <Find />
+    </div>
+  );
 }
 
 export default Search;
