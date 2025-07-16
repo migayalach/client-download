aee42088a07c66eb3745b849e4fbd1a733629e4e
migayalach <ayalachavezmiguel@gmail.com>
2025-07-15 23:50:31 -0400

    added styles

--- src/components/instructions/Instructions.tsx
+++ src/components/instructions/Instructions.tsx
@@ -4,24 +4,31 @@ import Text from "../text/Text";
 
 function Instructions() {
   return (
-    <div className="">
+    <div
+      id="instructions"
+      className="flex flex-col mt-4 mb-4 md:flex md:flex-row md:justify-evenly"
+    >
       <div id="container-1">
-        <h1>How to download videos or music from YouTube?</h1>
+        <h1 className="text-[20px] font-medium mb-2 md:text-[27px]">
+          How to download videos or music from YouTube?
+        </h1>
         {textAbout1.map(({ text }, index) => {
           return (
-            <>
-              <Text number={index + 1} text={text} key={index} />
-            </>
+            <div key={index}>
+              <Text number={index + 1} text={text} />
+            </div>
           );
         })}
       </div>
       <div id="container-2">
-        <h1>Characteristics:</h1>
+        <h1 className="text-[20px] font-medium mt-2 mb-2 md:mt-0 md:text-[27px]">
+          Characteristics:
+        </h1>
         {textAbout2.map(({ text }, index) => {
           return (
-            <>
-              <Text number={index + 1} text={text} key={index} />
-            </>
+            <div key={index}>
+              <Text number={index + 1} text={text} />
+            </div>
           );
         })}
       </div>
