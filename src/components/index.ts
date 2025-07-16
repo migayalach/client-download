6fa614f8c5eb24b7205f6e0c3978bdedf5ee35d5
migayalach <ayalachavezmiguel@gmail.com>
2025-07-15 23:49:44 -0400

    update components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,7 @@
-import NavBar from "./navBar/NavBar";
 import Search from "./search/Search";
 import Loading from "./loading/Loading";
 import Information from "./information/Information";
 import About from "./about/About";
-import Footer from "./footer/Footer";
 import Instructions from "./instructions/Instructions";
 
-export { NavBar, Search, Loading, Information, About, Footer, Instructions };
+export { Search, Loading, Information, About, Instructions };
