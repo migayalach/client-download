6bd81463a66a16f9bab3a398e1c6864044d75c7e
migayalach <ayalachavezmiguel@gmail.com>
2025-07-17 12:55:21 -0400

    CD

--- src/__tests__/instructions.spec.tsx
+++ src/__tests__/instructions.spec.tsx
@@ -1,2 +1,8 @@
 import { Instructions } from "@/components";
 import { textAbout1, textAbout2 } from "@/mocks/about";
+
+describe("My first Test", () => {
+    it("My first Test fake XD", () => {
+      expect(true).toBe(true);
+    });
+  });
