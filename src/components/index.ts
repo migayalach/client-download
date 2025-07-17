5a64d1e495a0b3517a8736d9ea214055e34509a9
migayalach <ayalachavezmiguel@gmail.com>
2025-07-17 12:48:45 -0400

    delete a component loading

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,7 @@
 import Search from "./search/Search";
-import Loading from "./loading/Loading";
 import Information from "./information/Information";
 import About from "./about/About";
 import Instructions from "./instructions/Instructions";
 import NavBar from "./navBar/NavBar";
 
-export { Search, Loading, Information, About, Instructions, NavBar };
+export { Search, Information, About, Instructions, NavBar };
