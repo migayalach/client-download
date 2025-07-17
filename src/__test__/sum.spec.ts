86182f7e3bc69fba3fe67869aa3d02b715f37958
migayalach <ayalachavezmiguel@gmail.com>
2025-07-17 00:28:00 -0400

    Subtract

--- src/__test__/sum.spec.ts
+++ src/__test__/sum.spec.ts
@@ -2,6 +2,14 @@ function sum(a: number, b: number): number {
   return a + b;
 }
 
+function res(a: number, b: number): number {
+  return a - b;
+}
+
 test("Adds 1 + 2 to equals 3", () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+test("Subtract 5 - 2 to equals 3", () => {
+  expect(res(5, 2)).toBe(3);
 });
