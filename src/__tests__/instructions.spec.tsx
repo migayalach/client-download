1f22ba8a28635d9beb4ce114dde04dcee2794b5b
migayalach <ayalachavezmiguel@gmail.com>
2025-07-21 21:48:09 -0400

    delete files

--- src/__tests__/instructions.spec.tsx
+++
@@ -1,8 +0,0 @@
-import { Instructions } from "@/components";
-import { textAbout1, textAbout2 } from "@/mocks/about";
-
-describe("My first Test", () => {
-    it("My first Test fake XD", () => {
-      expect(true).toBe(true);
-    });
-  });
